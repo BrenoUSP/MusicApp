d527619f76b7b0ee87ac82c989343a1c0bc19797
breno.almeida <brenoslivio@usp.br>
2018-06-17 16:14:40 -0300

    Telas inicial pronta com som.

--- Software/Musication/app/src/main/java/efestus/musication/MainActivity.java
+++ Software/Musication/app/src/main/java/efestus/musication/MainActivity.java
@@ -1,9 +1,14 @@
 package efestus.musication;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button but = (Button) findViewById(R.id.button);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.intro_sound);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

9673efd4a4757ecffbbe0d7fb0adb202641a4603
breno.almeida <brenoslivio@usp.br>
2018-07-01 05:18:25 -0300

    Trabalho com radio buttons.

--- Software/Musication/app/src/main/java/efestus/musication/lvl2_notmus_4.java
+++ Software/Musication/app/src/main/java/efestus/musication/lvl2_notmus_4.java
@@ -1,14 +1,37 @@
 package efestus.musication;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class lvl2_notmus_4 extends AppCompatActivity {
-    final int NUMBER_OF_RADIOBUTTONS_TO_ADD = 6;//Change it for other number of RadioButtons
-    RadioButton[] radioButton;
     RadioGroup radioGroup;
+    RadioButton radioBut1;
+    RadioButton radioBut2;
+    RadioButton radioBut3;
+    RadioButton radioBut4;
+    RadioButton radioBut5;
+    RadioButton radioBut6;
+
+    public int getRandomWithExclusion(Random rnd, int start, int end, int... exclude) {
+        int random = start + rnd.nextInt(end - start + 1 - exclude.length);
+        for (int ex : exclude) {
+            if (random < ex) {
+                break;
+            }
+            random++;
+        }
+        return random;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +40,56 @@ public class lvl2_notmus_4 extends AppCompatActivity {
 
         radioGroup = (RadioGroup) findViewById(R.id.radio_group);
 
-        RadioButton radioBut1 = (RadioButton) findViewById(R.id.radioButton);
-        RadioButton radioBut2 = (RadioButton) findViewById(R.id.radioButton2);
-        RadioButton radioBut3 = (RadioButton) findViewById(R.id.radioButton3);
-        RadioButton radioBut4 = (RadioButton) findViewById(R.id.radioButton4);
-        RadioButton radioBut5 = (RadioButton) findViewById(R.id.radioButton5);
-        RadioButton radioBut6 = (RadioButton) findViewById(R.id.radioButton6);
+        radioBut1 = new RadioButton(this);
+        radioBut1.setText("Notas acidentes, Nota, Acidente, G#, Ab");
 
-        radioGroup.addView(radioBut1);
-        radioGroup.addView(radioBut3);
-        radioGroup.addView(radioBut2);
-        radioGroup.addView(radioBut4);
-        radioGroup.addView(radioBut5);
-        radioGroup.addView(radioBut6);
+        radioBut2 = new RadioButton(this);
+        radioBut2.setText("Frequências acidentais, Acidente, Nota, #G, bA");
+
+        radioBut3 = new RadioButton(this);
+        radioBut3.setText("Notas acidentes, Nota, Acidente, G#, bA");
+
+        radioBut4 = new RadioButton(this);
+        radioBut4.setText("Frequências acidentais, Nota, Acidente, G#, Ab");
+
+        radioBut5 = new RadioButton(this);
+        radioBut5.setText("Notas agudas, Nota, Acidente, Gb, A#");
+
+        radioBut6 = new RadioButton(this);
+        radioBut6.setText("Notas agudas, Nota, Acidente, G#, Ab");
+
+        Random rand = new Random();
+
+        int num = rand.nextInt(3);
+
+        switch(num){
+            case 0:
+                radioGroup.addView(radioBut1, 0);
+                radioGroup.addView(radioBut3, 1);
+                radioGroup.addView(radioBut6, 2);
+                radioGroup.addView(radioBut2, 3);
+                radioGroup.addView(radioBut4, 4);
+                radioGroup.addView(radioBut5, 5);
+                break;
+            case 1:
+                radioGroup.addView(radioBut3, 0);
+                radioGroup.addView(radioBut1, 1);
+                radioGroup.addView(radioBut5, 2);
+                radioGroup.addView(radioBut2, 3);
+                radioGroup.addView(radioBut4, 4);
+                radioGroup.addView(radioBut6, 5);
+                break;
+            case 2:
+                radioGroup.addView(radioBut6, 0);
+                radioGroup.addView(radioBut4, 1);
+                radioGroup.addView(radioBut5, 2);
+                radioGroup.addView(radioBut2, 3);
+                radioGroup.addView(radioBut1, 4);
+                radioGroup.addView(radioBut3, 5);
+                break;
+        }
+
+
+
     }
 }
